d928227472c2aef27954f3aeda0cd65f296beffc
Marc Geweiler <marcgeweiler@MacBook-Pro-von-Marc.local>
2024-12-04 11:39:51 +0100

    spi test

--- Aufgabe 1/src/main.c
+++ Aufgabe 1/src/main.c
@@ -11,8 +11,11 @@ int main(void)
     uart_init_nucusb(115200);
     spi_init_adxl345();
 
-    int8_t buf[2]={0x00,0x00};
+    int8_t buf[2]={0x2D,0x08};
     spi_txrx(buf,2);
+
+    uint8_t values[] = {0xF2, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    spi_txrx(values, 7);
 
 
     while(1)
--- Aufgabe 1/src/spi.c
+++ Aufgabe 1/src/spi.c
@@ -13,6 +13,8 @@ int32_t spi_init_adxl345(void)
     GPIOA->MODER |= (GPIO_MODER_MODE5_1 | GPIO_MODER_MODE6_1 | GPIO_MODER_MODE7_1);     //Set alternate function mode
 
     GPIOA->AFR[0] &= ~((GPIO_AFRL_AFSEL5) + (GPIO_AFRL_AFSEL6) + (GPIO_AFRL_AFSEL7));      //Set alternate function 0 for PA05, PA06, PA07
+    //GPIOB->AFR[0] |= (5 << GPIO_AFRL_AFSEL5_Pos) | (5 << GPIO_AFRL_AFSEL6_Pos) | (5 << GPIO_AFRL_AFSEL7_Pos);
+
 
     //SPI Chip Select auf PA12  
     GPIOA->MODER |= GPIO_MODER_MODE12_0;
@@ -20,8 +22,9 @@ int32_t spi_init_adxl345(void)
 
     SPI1->CR1 &= ~SPI_CR1_SPE; //disable spi for config
 
+    SPI1->CR1 &= ~SPI_CR1_BR_2;
     SPI1->CR1 |=  SPI_CR1_BR_0; // 
-    
+    SPI1->CR1 |=  SPI_CR1_BR_1;
 
     SPI1->CR1 |= SPI_CR1_MSTR;  // Master Config
     SPI1->CR1 |= SPI_CR1_CPHA;  
@@ -45,17 +48,17 @@ int32_t spi_txrx(uint8_t *buf, uint32_t size)
         SPI1->DR = buf[i];     //transmit bit
         
         
-        while(!(SPI1->SR & SPI_SR_RXNE) || (SPI1->SR & SPI_SR_BSY))
+        while(!(SPI1->SR & SPI_SR_RXNE))
         {
             //wait until receive buffer no longer empty and communication finished
         }
         buf[i] = SPI1->DR;      //read received bit
     }
-    
+    while (SPI1->SR & SPI_SR_BSY);
+    {
+        // wait until whole transfer is done 
+    } 
     GPIOA->ODR |= GPIO_ODR_OD12; // unselect slave 
 
 }
 
-//Baurate (-> sensor max clock speed)
-//Bitwise communication
-//
