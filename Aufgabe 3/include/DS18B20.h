f59c1fea3ee22c8f3c2cae861b49e11930af724c
[Lino Sebeke] <[lino.sebeke@icloud.com]>
2024-12-19 11:57:48 +0100

    Kommunikation2

---
+++ Aufgabe 3/include/DS18B20.h
@@ -0,0 +1,10 @@
+#ifndef DS18B20_H
+#define DS18B20_H
+
+#include <stdint.h>
+
+
+
+
+
+#endif /*DS18B20_H*/
--- Aufgabe 3/include/ow.h
+++ Aufgabe 3/include/ow.h
@@ -1,6 +1,24 @@
 #ifndef OW_H
 #define OW_H
 
+ #define pull_down GPIOB->ODR &= ~(GPIO_ODR_OD13) 
+ #define pull_up GPIOB->ODR |= GPIO_ODR_OD13
+
+
+void ow_tim_init(void);
+
+void ow_init(void);
+
+void ow_timer(uint16_t time);
+
+void ow_reset(void);
+
+void ow_send_byte(uint8_t buffer);
+
+uint8_t ow_read_byte(void);
+
+void ow_strong_pullup(void);
+
 
 
 #endif /* OW_H */
