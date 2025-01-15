874ec65f01a9d565e335ae139556786606bd2d77
Marc Geweiler <Marcgeweiler@web.de>
2025-01-15 17:26:27 +0100

    oscillo Snip

--- Oszilloskop_1/Aufgabe 4/Oszilloskop_1/Oszilloskop_1/src/main.c
+++ Oszilloskop_1/Aufgabe 4/Oszilloskop_1/Oszilloskop_1/src/main.c
@@ -8,12 +8,19 @@
 volatile uint32_t ring_buffer[240];
 volatile uint8_t ring_counter = 0;
 volatile uint8_t falling_detect = 0;
+volatile uint8_t max_value = 0;
+volatile uint8_t middle_detect = 0; 
 volatile uint8_t draw_variable = 0;
-volatile uint8_t hysteresis_true = 0;
+volatile uint8_t start_countdown = 0;
+volatile uint8_t middle_id = 0;
+volatile uint8_t treshhold_counter = 120;
+volatile uint8_t zoom_pos = 1;
+uint32_t measure_count = 0;
 
 void OSC_gpio_init(void)
 {
-  RCC->IOPENR |= RCC_IOPENR_IOPCEN; // open GPIOC Channel
+  RCC->IOPENR |= RCC_IOPENR_IOPCEN;  // open GPIOC Channel
+  RCC->IOPENR |= RCC_IOPENR_GPIOBEN; // Enable SysClock for Port B
 
   GPIOC->MODER &= ~(GPIO_MODER_MODE4_1); // Set PC4 pins as output
   GPIOC->MODER |= GPIO_MODER_MODE4_0;
@@ -23,6 +30,15 @@ void OSC_gpio_init(void)
 
   GPIOC->MODER &= ~(GPIO_MODER_MODE6_1); // Set PC8 pins as output (LED D1)
   GPIOC->MODER |= GPIO_MODER_MODE6_0;
+
+  // Configure PB1/PB2 as input for button with pull-up resistor
+  GPIOB->MODER &= ~GPIO_MODER_MODE1;
+  GPIOB->PUPDR |= GPIO_PUPDR_PUPD1_0;
+  GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPD1_1);
+
+  GPIOB->MODER &= ~GPIO_MODER_MODE2;
+  GPIOB->PUPDR |= GPIO_PUPDR_PUPD2_0;
+  GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPD2_1);
 }
 
 void OSC_ADC_init(void)
@@ -88,7 +104,70 @@ void OSC_tim2_init(void)
   TIM2->CR1 |= TIM_CR1_CEN;
 }
 
-uint8_t TIM6_EN = 0;
+/**
+ * @brief  Configures external interrupt (EXTI) for button on PB1 & PB2
+ */
+void EXTI_init()
+{
+  RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; // Enable SysClock for SYSCFG
+
+  SYSCFG->EXTICR[0] = SYSCFG_EXTICR1_EXTI1_PB + SYSCFG_EXTICR1_EXTI2_PB; // Map EXTI to PB1 & PB2
+
+  EXTI->IMR |= EXTI_IMR_IM2; // Enable interrupt mask on line 1&2
+  EXTI->IMR |= EXTI_IMR_IM1;
+
+  EXTI->FTSR |= EXTI_FTSR_TR2; // Configure falling edge trigger for line 1&2
+  EXTI->FTSR |= EXTI_FTSR_TR1;
+
+  // Set interrupt priority & enable EXTI interrupt in NVIC
+  NVIC_SetPriority(EXTI0_1_IRQn, 1);
+  NVIC_EnableIRQ(EXTI0_1_IRQn);
+  NVIC_SetPriority(EXTI2_3_IRQn, 2);
+  NVIC_EnableIRQ(EXTI2_3_IRQn);
+}
+
+/**
+ * @brief Handles external interrupt for EXTI lines 0-1
+ *        Toggles the car_loop flag when button on PB1 is pressed
+ */
+void EXTI0_1_IRQHandler(void)
+{
+  if (EXTI->PR & EXTI_PR_PIF1)
+  { // Check if interrupt was triggered on line 1
+    if (zoom_pos < 1)
+    {
+      systick_delay_ms(70); // Debounce delay
+    }
+    else
+    {
+      zoom_pos -= 1;
+      systick_delay_ms(70); // Debounce delay
+    }
+  }
+  EXTI->PR = EXTI_PR_PIF1; // Clear pending interrupt flag on line 1
+}
+
+/**
+ * @brief Handles external interrupt for EXTI lines 2-3
+ *        Toggles the pedestrian_loop flag when button on PB2 is pressed
+ */
+void EXTI2_3_IRQHandler(void)
+{
+  if (EXTI->PR & EXTI_PR_PIF2)
+  { // Check if interrupt was triggered on line 2
+    if (zoom_pos >= 4)
+    {
+      systick_delay_ms(70); // Debounce delay
+    }
+    else
+    {
+      zoom_pos += 1;
+      systick_delay_ms(70); // Debounce delay
+    }
+  }
+
+  EXTI->PR = EXTI_PR_PIF2; // Clear pending interrupt flag on line 2
+}
 
 void TIM6_DAC_IRQHandler(void)
 {
@@ -100,7 +179,6 @@ void TIM6_DAC_IRQHandler(void)
   TIM6->SR &= ~TIM_SR_UIF; // Clear the interrupt flag
 }
 
-uint8_t TIM2_EN = 0;
 void TIM2_IRQHandler(void)
 {
   if (TIM2->SR & TIM_SR_UIF) // Check if update interrupt flag is set
@@ -116,40 +194,51 @@ void TIM2_IRQHandler(void)
       }
 
       ring_buffer[ring_counter] = ADC1->DR;
+      measure_count += 1;
 
-    }
-
-      if (ring_counter > 100)
+      if (measure_count>=120)
       {
-         if (ring_counter < 119)
-      {
-        if (ring_buffer[ring_counter] >= 4060)
+        if (((ring_buffer[ring_counter-1])-(ring_buffer[ring_counter]))>=50 && falling_detect==0)
         {
-          hysteresis_true = 1;
+          falling_detect = 1;
+          max_value = (ring_counter-1);
+        }
+
+        if(falling_detect == 1 && (ring_buffer[ring_counter] < 2048) && (ring_buffer[ring_counter-1] >= 2048) && start_countdown==0)
+        {
+          start_countdown = 1;
+          middle_id = ring_counter;
+        }
+
+        
+      }
+
+      if(start_countdown == 1)
+      {
+        if(treshhold_counter<=0)
+        {
+          draw_variable = 1;
+        }
+        else
+        {
+          treshhold_counter -=1;
         }
       }
-      }
 
-      if (ring_counter > 119 && ring_counter < 140)
+     ring_counter = (ring_counter +=1) % 240; 
+      
+    
+      /*if (ring_counter >= 120 && (ring_buffer[ring_counter] < 2048) && (ring_buffer[ring_counter-1] >= 2048))
       {
 
-        if (ring_buffer[ring_counter] <= 2589)
-        {
-          if (ring_buffer[ring_counter] >= 2530)
-          {
-            if (hysteresis_true == 1)
-            {
-              falling_detect = 1;
-            }
-          }
-        }
+        falling_detect = 1;
       }
 
       if (ring_counter == 240)
       {
         if (falling_detect == 1)
         {
-          draw_variable = 1;          
+          draw_variable = 1;
         }
 
         ring_counter = 0;
@@ -157,12 +246,81 @@ void TIM2_IRQHandler(void)
       else
       {
         ring_counter += 1;
-      }
-  
+      }*/
+
+    
+      
+    }
   }
 
   TIM2->SR &= ~TIM_SR_UIF; // Clear the interrupt flag
 }
+
+void draw_funciton(void)
+{
+
+  if (draw_variable == 1)
+  {
+    ili9341_rect_fill(0, 0, 240, 320, ILI9341_COLOR_WHITE);
+    if ((middle_id-120)<0)
+    {
+      iterator = middle_id+120;
+      upper_limit = 240- middle_id+120;
+    } 
+    else
+    {
+      iterator = middle_id-120;
+      upper_limit =
+    }
+    
+    for (uint8_t i = 0; i < upper_limit; i++)
+    { 
+      ili9341_pixel_set(i, 150 - ((ring_buffer[iterator]) / 64), ILI9341_COLOR_BLACK);
+      ili9341_pixel_set(i, 149 - ((ring_buffer[iterator]) / 64), ILI9341_COLOR_BLACK);
+      iterator +=1;
+    }
+      zoom_function();
+      draw_variable = 0;
+      falling_detect = 0;
+      start_countdown = 0;
+      measure_count = 0;
+      treshhold_counter = 120;
+
+      
+  }
+  
+}
+
+void zoom_function(void)
+{
+      if (zoom_pos == 4)
+    {
+      TIM2->ARR = 1600;
+    }
+
+    if (zoom_pos == 3)
+    {
+      TIM2->ARR = 800;
+    }
+
+    if (zoom_pos == 2)
+    {
+      TIM2->ARR = 600;
+    }
+
+    if (zoom_pos == 1)
+    {
+      TIM2->ARR = 400;
+    }
+
+    if (zoom_pos == 0)
+    {
+      TIM2->ARR = 200;
+    }
+  }
+
+
+
 
 int main(void)
 {
@@ -174,23 +332,16 @@ int main(void)
   OSC_tim6_init();
   OSC_tim2_init();
   ili9341_init(0);
+  EXTI_init();
 
   ili9341_rect_fill(0, 0, 240, 320, ILI9341_COLOR_WHITE);
 
   while (1)
   {
+    draw_funciton();
+   
 
-    if (draw_variable == 1)
-    {   
-      ili9341_rect_fill(0, 0, 240, 320, ILI9341_COLOR_WHITE);
-      for (uint8_t i = 0; i < 240; i++)
-      {
-        ili9341_pixel_set(i, 220 - ((ring_buffer[i]) / 64), ILI9341_COLOR_BLACK);
-        ili9341_pixel_set(i, 219 - ((ring_buffer[i]) / 64), ILI9341_COLOR_BLACK);
-      }
-      draw_variable = 0;
-      falling_detect = 0;
-      hysteresis_true = 0;
-    }
-  }
+  
+}
+
 }
